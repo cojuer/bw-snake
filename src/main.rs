6b125227099997083f4e88d8c0413610f86a4ac8
cojuer <cojuer@gmail.com>
2022-07-24 09:21:45 +0300

    Allow snake to move
    Add controls of snake direction
    Move snake using FixedTimestep
    Add snake body segments
    Add checking collisions
    Move update_position to later stage to fix reaction to spawning

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use bevy::core::FixedTimestep;
 use bevy::prelude::*;
 use std::collections::HashMap;
 
-#[derive(Component)]
+#[derive(Copy, Clone, Component, PartialEq, Eq)]
 pub struct Pos {
     pub x: u32,
     pub y: u32,
@@ -46,18 +47,36 @@ pub struct Scene {
     y_size: u32,
 }
 
-fn add_snake(mut commands: Commands, asset_server: Res<AssetServer>) {
+const Z_SNAKE: f32 = 10.0;
+
+fn spawn_snake(mut commands: Commands, asset_server: Res<AssetServer>) {
     let snake_image = asset_server.load("images/snake.png");
     commands
         .spawn()
         .insert(Snake)
         .insert(SnakeMeta {
-            len: 3,
+            len: 4,
             dir: Direction::Right,
         })
+        .insert(Collision)
         .insert(Pos { x: 5, y: 5 })
         .insert_bundle(SpriteBundle {
             texture: snake_image,
+            ..Default::default()
+        });
+}
+
+fn spawn_snake_body(mut commands: Commands, asset_server: Res<AssetServer>, pos: &Pos) {
+    let body_image = asset_server.load("images/snake.png");
+    commands
+        .spawn()
+        .insert(SnakeBody)
+        .insert(Age(0))
+        .insert(Pos { x: pos.x, y: pos.y })
+        .insert(Collision)
+        .insert_bundle(SpriteBundle {
+            texture: body_image,
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, Z_SNAKE)),
             ..Default::default()
         });
 }
@@ -66,6 +85,15 @@ fn add_snake(mut commands: Commands, asset_server: Res<AssetServer>) {
 pub enum TileType {
     Wall,
     Floor,
+}
+
+impl TileType {
+    fn has_collision(&self) -> bool {
+        match self {
+            TileType::Wall => true,
+            TileType::Floor => false,
+        }
+    }
 }
 
 pub struct TileFactory {
@@ -87,13 +115,15 @@ impl TileFactory {
     pub fn spawn(&self, commands: &mut Commands, pos: Pos, tile: TileType) -> Entity {
         let material = self.materials.get(&tile).unwrap_or(&self.err_material);
 
-        commands
-            .spawn_bundle(SpriteBundle {
-                texture: material.clone(),
-                ..Default::default()
-            })
-            .insert_bundle((Tile, pos, Collision))
-            .id()
+        let mut ent_cmd = commands.spawn_bundle(SpriteBundle {
+            texture: material.clone(),
+            ..Default::default()
+        });
+        ent_cmd.insert_bundle((Tile, pos));
+        if tile.has_collision() {
+            ent_cmd.insert(Collision);
+        }
+        ent_cmd.id()
     }
 }
 
@@ -133,6 +163,78 @@ fn create_basic_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.insert_resource(scene);
 }
 
+fn move_snake(
+    commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut query: Query<(&SnakeMeta, &mut Pos)>,
+) {
+    let (snake_meta, mut pos) = query.single_mut();
+    let old_pos = *pos;
+    match snake_meta.dir {
+        Direction::Up => {
+            pos.y += 1;
+        }
+        Direction::Down => {
+            pos.y -= 1;
+        }
+        Direction::Left => {
+            pos.x -= 1;
+        }
+        Direction::Right => {
+            pos.x += 1;
+        }
+    }
+
+    spawn_snake_body(commands, asset_server, &old_pos);
+}
+
+fn check_snake_collides(
+    snake_query: Query<(Entity, &SnakeMeta, &Pos)>,
+    collision_query: Query<(Entity, &Pos, Option<&Age>), With<Collision>>,
+) {
+    let (snake_id, snake_meta, snake_pos) = snake_query.single();
+    for (ent_id, ent_pos, opt_age) in collision_query.iter() {
+        // TODO: probably move system into another stage
+        // so that tail despawns before we check collision
+        if let Some(age) = opt_age {
+            if age.0 + 1 == snake_meta.len {
+                continue;
+            }
+        }
+        if snake_pos == ent_pos && snake_id != ent_id {
+            println!("failed");
+        }
+    }
+}
+
+fn despawn_old(
+    mut commands: Commands,
+    mut body_query: Query<(Entity, &SnakeBody, &mut Age)>,
+    snake_query: Query<&SnakeMeta>,
+) {
+    let snake_meta = snake_query.single();
+    for (entity, _, mut age) in body_query.iter_mut() {
+        age.as_mut().0 += 1;
+        if age.0 + 1 == snake_meta.len {
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+fn control_snake(mut snake_query: Query<&mut SnakeMeta>, inputs: Res<Input<KeyCode>>) {
+    let mut snake_meta = snake_query.single_mut();
+
+    if inputs.is_changed() {
+        match inputs.get_just_released().next() {
+            Some(KeyCode::Up | KeyCode::W) => snake_meta.dir = Direction::Up,
+            Some(KeyCode::Down | KeyCode::S) => snake_meta.dir = Direction::Down,
+            Some(KeyCode::Left | KeyCode::A) => snake_meta.dir = Direction::Left,
+            Some(KeyCode::Right | KeyCode::D) => snake_meta.dir = Direction::Right,
+            _ => {}
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -141,8 +243,21 @@ fn main() {
             commands.spawn_bundle(UiCameraBundle::default());
         })
         .add_startup_system(create_basic_scene)
-        .add_startup_system(add_snake)
+        .add_startup_system(spawn_snake)
         .add_system(bevy::input::system::exit_on_esc_system)
-        .add_system(update_position)
+        .add_system(control_snake)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(1.0))
+                .with_system(move_snake)
+                .with_system(despawn_old.after(move_snake))
+                .with_system(check_snake_collides.after(despawn_old)),
+        )
+        // Moving snake is implemented by spawning body segments, we need
+        // either to set correct transform or to update transform after
+        // spawning. As spawning happens in the end of the stage we run system
+        // updating transforms in a new stage.
+        // TODO: add custom stage between Update and PostUpdate
+        .add_system_to_stage(CoreStage::PostUpdate, update_position)
         .run();
 }
